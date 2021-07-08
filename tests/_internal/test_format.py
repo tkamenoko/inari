563e0fd04199f5b2487d0c973865785af4feaddf
T. Kameyama <tkamenoko@vivaldi.net>
2021-07-08 18:51:46 +0900

    fix: grouping fixtures by NamedTuple

--- tests/_internal/test_format.py
+++ tests/_internal/test_format.py
@@ -1,5 +1,43 @@
+from typing import NamedTuple
+
 from inari._internal import _format
 from ward import each, test, using
+
+
+class Fixtures(NamedTuple):
+    raw_description: str
+    attributes: str
+    result: str
+
+
+simple = Fixtures(
+    raw_description="* var (`object`): Target object.",
+    attributes="",
+    result="* **var** (`object`): Target object.",
+)
+attr_and_emphasize = Fixtures(
+    raw_description="* variables (`list[VariableCollector]`):"
+    + " Class properties. **IMPORTANT!**",
+    attributes="{: #ClassItself }",
+    result="* **variables**{: #ClassItself } (`list[VariableCollector]`): Class "
+    + "properties. **IMPORTANT!**",
+)
+multiline = Fixtures(
+    raw_description="* var (`object`): Target object.\n"
+    + "* var (`object`): `backtick`\n\n"
+    + "**Returns**\n"
+    + "* `str`: Return type.\n",
+    attributes="",
+    result="* **var** (`object`): Target object.\n"
+    + "* **var** (`object`): `backtick`\n\n"
+    + "**Returns**\n"
+    + "* `str`: Return type.\n",
+)
+no_description = Fixtures(
+    raw_description="* no_description(`str`)",
+    attributes="",
+    result="* **no_description** (`str`)",
+)
 
 
 @test(
@@ -8,24 +46,22 @@ from ward import each, test, using
 )
 @using(
     raw_description=each(
-        "* var (`object`): Target object.",
-        "* variables (`list[VariableCollector]`): Class properties. **IMPORTANT!**",
-        "* var (`object`): Target object.\n"
-        + "* var (`object`): `backtick`\n\n"
-        + "**Returns**\n"
-        + "* `str`: Return type.\n",
-        "* no_description(`str`)",
+        simple.raw_description,
+        attr_and_emphasize.raw_description,
+        multiline.raw_description,
+        no_description.raw_description,
     ),
-    attributes=each("", "{: #ClassItself }", "", ""),
+    attributes=each(
+        simple.attributes,
+        attr_and_emphasize.attributes,
+        multiline.attributes,
+        no_description.attributes,
+    ),
     result=each(
-        "* **var** (`object`): Target object.",
-        "* **variables**{: #ClassItself } (`list[VariableCollector]`): Class "
-        + "properties. **IMPORTANT!**",
-        "* **var** (`object`): Target object.\n"
-        + "* **var** (`object`): `backtick`\n\n"
-        + "**Returns**\n"
-        + "* `str`: Return type.\n",
-        "* **no_description** (`str`)",
+        simple.result,
+        attr_and_emphasize.result,
+        multiline.result,
+        no_description.result,
     ),
 )
 def _(raw_description: str, attributes: str, result: str) -> None:
--- tests/_internal/test_path.py
+++ tests/_internal/test_path.py
@@ -1,13 +1,40 @@
-from ward import test, using, each
+from typing import NamedTuple
 
 from inari._internal import _path
+from ward import each, test, using
+
+
+class Fixtures(NamedTuple):
+    page: str
+    link_to: str
+    result: str
+
+
+current_dir = Fixtures(page="/inari/foo/bar", link_to="/inari/foo/baz", result="baz")
+link_self = Fixtures(page="/inari/foo/bar", link_to="/inari/foo/bar", result="")
+another_dir = Fixtures(page="/inari/foo/bar", link_to="/inari/baz", result="../baz")
+another_dir_child = Fixtures(
+    page="/inari/foo/bar", link_to="/inari/baz/spam", result="../baz/spam"
+)
 
 
 @test("Relative link from `{page}` to `{link_to}` should be `{result}` .")
 @using(
-    page="/inari/foo/bar",
-    link_to=each("/inari/foo/baz", "/inari/foo/bar", "/inari/baz/spam", "/inari/baz"),
-    result=each("baz", "", "../baz/spam", "../baz"),
+    page=each(
+        current_dir.page, link_self.page, another_dir.page, another_dir_child.page
+    ),
+    link_to=each(
+        current_dir.link_to,
+        link_self.link_to,
+        another_dir.link_to,
+        another_dir_child.link_to,
+    ),
+    result=each(
+        current_dir.result,
+        link_self.result,
+        another_dir.result,
+        another_dir_child.result,
+    ),
 )
 def _(page: str, link_to: str, result: str) -> None:
     assert _path.get_relative_path(page, link_to) == result
